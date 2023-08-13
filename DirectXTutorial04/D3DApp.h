90e743923a913f35e0123148d793b31095c7b4f6
shinjeongmin <sin37minduke@naver.com>
2023-08-13 12:58:38 +0900

    Upload legacy tutorial files.

--- DirectXTutorial04/D3DApp.h
+++ DirectXTutorial04/D3DApp.h
@@ -3,7 +3,9 @@
 #include "Window.h"
 #include <d3d11.h>
 
-//#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "d3d11.lib")
+#pragma comment(lib, "dxgi.lib")
+#pragma comment(lib, "d3dcompiler.lib")
 
 class D3DApp
 {
